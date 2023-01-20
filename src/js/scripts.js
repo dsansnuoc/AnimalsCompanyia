45af36d392c8eae9f4d0d6c212f5bdb825ec958f
dsansnuoc - David Sans Naudó <dsans@wordplex.es>
2023-01-20 05:00:02 +0100

    PAC 3

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 import "./import-jquery.js";
 
 $(document).ready(main);
+// $(document).ready(img);
 
 let menuBar = true;
 
@@ -25,3 +26,26 @@ function main() {
         menuBar = !menuBar;
     });
 };
+
+
+function img() {
+
+    if ("loading" in HTMLImageElement.prototype) {
+      // Si el navegador soporta lazy-load, tomamos todas las imágenes que tienen la clase
+      // `lazyload`, obtenemos el valor de su atributo `data-src` y lo inyectamos en el `src`.
+      const images = document.querySelectorAll("img");
+      images.forEach((img) => {
+        var timestamp = new Date().getTime();  
+        console.log(img.dataset.src);
+        img.src = img.dataset.src+'?t=' + timestamp;
+        
+      });
+    } else {
+      // Importamos dinámicamente la libreria `lazysizes`
+      let script = document.createElement("script");
+      script.async = true;
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
+      document.body.appendChild(script);
+    }
+}
